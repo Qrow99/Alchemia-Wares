4747ab370d8b82316be13089cd38873e4bb5532c
Qrow99 <bkeyro99@gmail.com>
2021-11-01 15:48:06 -0700

    Slightly tweaked the main menu spawn items
    Tweaked spawning items on main menu to not have collision or rigidbodies to increase performance

--- Assets/Scripts/rotate_camera.cs
+++ Assets/Scripts/rotate_camera.cs
@@ -19,6 +19,10 @@ public class rotate_camera : MonoBehaviour
             int j = Random.Range(0, Potions.Length-1);
             GameObject new_potion = Instantiate( Potions[j], Random.insideUnitSphere * radius, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
             new_potion.GetComponent<Rigidbody>().isKinematic = true;
+            if(new_potion.GetComponent<Collider>())
+            {
+                new_potion.GetComponent<Collider>().enabled = false;
+            }
         }
     }
     // Update is called once per frame
