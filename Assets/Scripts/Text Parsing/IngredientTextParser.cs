fb8459a9d2cb5e7831e88a93ed056c449ae8ba0b
pjheric <pjheric1219@gmail.com>
2021-11-10 19:43:33 -0800

    text parser framework

--- Assets/Scripts/Text Parsing/IngredientTextParser.cs
+++ Assets/Scripts/Text Parsing/IngredientTextParser.cs
@@ -6,57 +6,68 @@ using System.Linq;
 using UnityEngine.UI;
 using TMPro; 
 
-public class TextParser : MonoBehaviour
+public class IngredientTextParser : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI ingredientName;
-    [SerializeField] TextMeshProUGUI shortDesc;
-    [SerializeField] TextMeshProUGUI longDesc;
-    [SerializeField] TextMeshProUGUI processingMethod;
+    //Witchipedia displays 2 ingredients per page
+    [SerializeField] TextMeshProUGUI ingredientName1;
+    [SerializeField] TextMeshProUGUI shortDesc1;
+    [SerializeField] TextMeshProUGUI longDesc1;
+    [SerializeField] TextMeshProUGUI processingMethod1;
+
+    [SerializeField] TextMeshProUGUI ingredientName2;
+    [SerializeField] TextMeshProUGUI shortDesc2;
+    [SerializeField] TextMeshProUGUI longDesc2;
+    [SerializeField] TextMeshProUGUI processingMethod2;
     public Button prevButton;
     public Button nextButton; 
     public List<string[]> dataSheet = new List<string[]>();
-    public int counter = 0; 
+    public int counter = 0; //Essentially just page number
     public void Start()
     {
+        //Initialize the buttons
         Button next = nextButton.GetComponent<Button>();
         Button prev = prevButton.GetComponent<Button>();
         next.onClick.AddListener(GoNext);
-        prev.onClick.AddListener(GoPrev); 
-        string readFromFilePath = Application.dataPath + "/Text Sample Stuff/" + "Ingredient Descriptions - Sheet1" + ".txt";
+        prev.onClick.AddListener(GoPrev);
+        //Get the file, slice it into a list of strings
+        string readFromFilePath = Application.dataPath + "/Imported Text Assets/" + "Ingredient Descriptions" + ".txt";
         List<string> fileLines = File.ReadAllLines(readFromFilePath).ToList();
         foreach(string line in fileLines)
         {
             string[] content = line.Split('\t');
             dataSheet.Add(content); 
         }
-        ingredientName.text = dataSheet[counter][0];
-        shortDesc.text = dataSheet[counter][1];
-        longDesc.text = dataSheet[counter][2];
-        processingMethod.text = dataSheet[counter][3];
+        //Draw the initial text
+        DrawText(); 
     }
+    //LINK THIS UP WITH REP SYSTEM
     public void GoNext()
     {
-        print("next");
-        if(counter < dataSheet.Count()-1)
+        if(counter < dataSheet.Count()/2-1)
         {
             counter += 1;
-            ingredientName.text = dataSheet[counter][0];
-            shortDesc.text = dataSheet[counter][1];
-            longDesc.text = dataSheet[counter][2];
-            processingMethod.text = dataSheet[counter][3];
+            DrawText(); 
         }
     }
     public void GoPrev()
     {
-        print("prev");
         if (counter > 0)
         {
             counter -= 1;
-            ingredientName.text = dataSheet[counter][0];
-            shortDesc.text = dataSheet[counter][1];
-            longDesc.text = dataSheet[counter][2];
-            processingMethod.text = dataSheet[counter][3];
+            DrawText(); 
         }
+    }
+    private void DrawText()
+    {
+        ingredientName1.text = dataSheet[counter][0];
+        shortDesc1.text = dataSheet[counter][1];
+        longDesc1.text = dataSheet[counter][2];
+        processingMethod1.text = dataSheet[counter][3];
+
+        ingredientName2.text = dataSheet[counter + 1][0];
+        shortDesc2.text = dataSheet[counter + 1][1];
+        longDesc2.text = dataSheet[counter + 1][2];
+        processingMethod2.text = dataSheet[counter + 1][3];
     }
     
 }
--- Assets/Scripts/Text Parsing/PotionTextParser.cs
+++ Assets/Scripts/Text Parsing/PotionTextParser.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using TMPro;
 
 public class PotionTextParser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Basically identical to ingredient text parser
+    [SerializeField] TextMeshProUGUI potionName1;
+    [SerializeField] TextMeshProUGUI shortDesc1;
+    [SerializeField] TextMeshProUGUI longDesc1;
+    [SerializeField] TextMeshProUGUI potionEquation1;
+
+    [SerializeField] TextMeshProUGUI potionName2;
+    [SerializeField] TextMeshProUGUI shortDesc2;
+    [SerializeField] TextMeshProUGUI longDesc2;
+    [SerializeField] TextMeshProUGUI potionEquation2;
+    public Button prevButton;
+    public Button nextButton;
+    public List<string[]> dataSheet = new List<string[]>();
+    public int counter = 0; //Essentially just page number
+    public void Start()
     {
-        
+        //Initialize the buttons
+        Button next = nextButton.GetComponent<Button>();
+        Button prev = prevButton.GetComponent<Button>();
+        next.onClick.AddListener(GoNext);
+        prev.onClick.AddListener(GoPrev);
+        //Get the file, slice it into a list of strings
+        string readFromFilePath = Application.dataPath + "/Imported Text Assets/" + "Potion Descriptions" + ".txt";
+        List<string> fileLines = File.ReadAllLines(readFromFilePath).ToList();
+        foreach (string line in fileLines)
+        {
+            string[] content = line.Split('\t');
+            dataSheet.Add(content);
+        }
+        //Draw the initial text
+        DrawText();
+    }
+    //LINK THIS UP WITH REP SYSTEM
+    public void GoNext()
+    {
+        if (counter < dataSheet.Count() / 2 - 1)
+        {
+            counter += 1;
+            DrawText();
+        }
+    }
+    public void GoPrev()
+    {
+        if (counter > 0)
+        {
+            counter -= 1;
+            DrawText();
+        }
+    }
+    private void DrawText()
+    {
+        potionName1.text = dataSheet[counter][0];
+        shortDesc1.text = dataSheet[counter][1];
+        longDesc1.text = dataSheet[counter][2];
+        potionEquation1.text = dataSheet[counter][3];
+
+        potionName2.text = dataSheet[counter + 1][0];
+        shortDesc2.text = dataSheet[counter + 1][1];
+        longDesc2.text = dataSheet[counter + 1][2];
+        potionEquation2.text = dataSheet[counter + 1][3];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
